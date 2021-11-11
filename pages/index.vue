fdc4b29a038ec4a0822903848909b49d7e007d6a
Zach Edwards <zachary@zachs-mbp.lan>
2021-11-11 18:01:23 -0500

    pagination notes

--- pages/index.vue
+++ pages/index.vue
@@ -54,7 +54,23 @@ export default {
     }
   },
 
+  computed: {
+    indexStart() {
+      return(this.current - 1) * this.pageSize
+    },
+    indexEnd() {
+      return this.indexStart + this.pageSize 
+    },
+    paginatedStories: function () {
+      return this.storiesWithData.slice(this.indexStart, this.indexEnd)
+    },
+    filteredStories: function () {
+      return this.storiesWithData
+    }
+  },
+
   methods: {
+    // should parameter be value = 1 ?
     handlePageChange(value) {
       this.current = value
       this.getStories()
@@ -70,6 +86,8 @@ export default {
 
     async getStories() {
       let stories = []
+
+      // let initialStories = []
 
       let all_pages = await axios.get(
         `/auth/spaces/${this.$route.query.space_id}/stories?per_page=${this.perPage}&page=${this.page}`
@@ -92,12 +110,14 @@ export default {
                     plugin: "meta-fields",
                   };
                 }
-                // this.storiesWithData.push(...this.initialStories(JSON.parse(JSON.stringify(res.data.story)))
-                // when i don't parse res.data.story i don't get the story data and stories don't display
                 this.storiesWithData.push(JSON.parse(JSON.stringify(res.data.story))
+
+                // ^ when res.data.story isn't parsed and stringified the story data and stories don't display
+                // should i take the initialStories array from above and push 
+                // the storiesWithData into the initialStories like so: 
+                // this.storiesWithData.push(...this.initialStories(JSON.parse(JSON.stringify(res.data.story)))
                 );
               }
-                // stories.push(...this.initialStories(JSON.parse(JSON.stringify(res.data.story))))
             })
             .catch((error) => {
               console.log(error);
@@ -105,21 +125,6 @@ export default {
         })
       );
     },
-  },
-
-  computed: {
-    indexStart() {
-      return(this.current - 1) * this.pageSize
-    },
-    indexEnd() {
-      return this.indexStart + this.pageSize 
-    },
-    paginatedStories: function () {
-      return this.storiesWithData.slice(this.indexStart, this.indexEnd)
-    },
-    filteredStories: function () {
-      return this.storiesWithData
-    }
   },
 };
 </script>
